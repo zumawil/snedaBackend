92c88a7be0ff8aff701ebddc50dbeec607ca537f
william <willadan667@gmail.com>
2025-11-13 10:01:54 +0000

    minor change

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('categories/', views.CategoryListCreateView.as_view(), name='categories'),
-    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
+    path('categories/', views.CategoryListCreateView.as_view(), name='categories'), # list all categories and create a new category
+    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'), # get a category by id
     path('products/', views.ProductListView.as_view(), name='products'), # list all products
     path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'), # get a product by id
     path('product-images/', views.ProductImageView.as_view(), name='product-images'), # list all product images
