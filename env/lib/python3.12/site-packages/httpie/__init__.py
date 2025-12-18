379e6a52e7d0b12addb87ec90b3ae938eff80f5e
william <willadan667@gmail.com>
2025-12-18 16:30:00 +0000

    untracking gitignore files

--- env/lib/python3.12/site-packages/httpie/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-HTTPie: modern, user-friendly command-line HTTP client for the API era.
-
-"""
-
-__version__ = '3.2.4'
-__date__ = '2024-11-01'
-__author__ = 'Jakub Roztocil'
-__licence__ = 'BSD'
